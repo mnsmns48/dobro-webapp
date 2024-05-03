e28a86fe1fb710d09e2e50ef89fb3fa7904bc802
mnsmns48 <getzboy@list.ru>
2024-05-03 18:05:40 +0300

    add if __name__ == "__main__":

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from starlette.staticfiles import StaticFiles
 from routers import pages_router
@@ -6,3 +7,7 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory=f"static"), name="static")
 app.include_router(pages_router, tags=["page"])
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", port=5000)
